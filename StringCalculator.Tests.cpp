ee3b8fee563e2d12a1ce2b7196e9c728c3b871a8
suhasi-pm <pmsuhasi@gmail.com>
2024-10-28 14:46:54 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -1,1 +1,16 @@
+#include "StringCalculator.h"
+#include <gtest/gtest.h>
 
+TEST(string_calculator_add, when_passed_a_single_number_returns_0_for_zero)
+{
+  //Arrange
+  StringCalculator objUnderTest;
+  sting input="";
+  int expectedValue=0;
+
+  //Act
+  int actualValue=objUnderTest.Add(input);
+
+  //ASSERT
+  ASSERT_EQ(actualValue, expectedValue);
+}
