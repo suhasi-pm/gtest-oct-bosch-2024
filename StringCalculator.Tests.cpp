fe458d0e5d994cc7c70c71e506b33cf34562379c
suhasi-pm <pmsuhasi@gmail.com>
2024-10-28 14:48:08 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -5,7 +5,7 @@ TEST(string_calculator_add, when_passed_a_single_number_returns_0_for_zero)
 {
   //Arrange
   StringCalculator objUnderTest;
-  sting input="";
+  string input="";
   int expectedValue=0;
 
   //Act
