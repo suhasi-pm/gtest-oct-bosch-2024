87727f60d4aca3252775f44a80555b24d0cd4d3e
suhasi-pm <pmsuhasi@gmail.com>
2024-10-29 15:57:15 +0530

    Create VehicleSpeedMonitor.h

---
+++ speedMonitor/VehicleSpeedMonitor.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <random> 
+#include <string>
+#include "ILogger.h"
+#include "IMessageCommunicator.h"
+#include "ISpeedSensor.h"
+using namespace std;
+
+class SpeedMonitor
+{
+    private : int _speedThreshold;
+    public:
+        SpeedMonitor(int speedThreshold): _speedThreshold{speedThreshold} {}
+        int getSpeedThreshold() const
+        {
+            return this->_speedThreshold;
+        }
+        //Dependency Injection 
+        void monitor(IMessageCommunicator* communicator, ISpeedSensor* sensor, ILogger* logger);
+};
