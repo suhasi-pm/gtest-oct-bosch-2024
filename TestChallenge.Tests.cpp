574d8d30d7dbfc4d05ffa58621911bde1e7403f8
suhasi-pm <pmsuhasi@gmail.com>
2024-10-29 11:53:48 +0530

    Update TestChallenge.Tests.cpp

--- TestChallenge.Tests.cpp
+++ TestChallenge.Tests.cpp
@@ -3,16 +3,19 @@
 #include <tuple>
 using namespace std;
 
-class TestChallengeFixture:public testing::Test{
+class TestChallengeFixture:public testing::Test
+{
   protected:
     TestChallenge *objUnderTest;
-  void SetUp() override {
+  void SetUp() override
+  {
       result = 0;
   }
 };
 
 
-TEST_F(TestChallengeFixture){
+TEST_F(TestChallengeFixture, AddTwoNumbers)
+{
     int expectedValue = 9;
     add(3, 6, result);
     ASSERT_EQ(result, expectedValue);
