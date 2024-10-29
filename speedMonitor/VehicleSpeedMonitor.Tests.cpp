9eff2a152ae60fb3a739709e9918f069e3b8dcf7
suhasi-pm <pmsuhasi@gmail.com>
2024-10-29 18:04:01 +0530

    Update VehicleSpeedMonitor.Tests.cpp

--- speedMonitor/VehicleSpeedMonitor.Tests.cpp
+++ speedMonitor/VehicleSpeedMonitor.Tests.cpp
@@ -1,9 +1,52 @@
 #include "VehicleSpeedMonitor.h"
 #include <gtest/gtest.h>
 
-TEST(VehicleSpeedMonitorTestSuite, AssertValidInstanceWithSpeedThreshold)
+//Hand Made Mock
+class MockLogger:public ILogger
 {
-    int speedThreshold = 70;  // Sample threshold
-    SpeedMonitor monitor{speedThreshold};
-    EXPECT_EQ(monitor.getSpeedThreshold(), speedThreshold);
+  public:
+    int writeMethodCallCount=0;
+    string  writeMethodArg;
+    void write(const string& message)
+    {
+        this->writeMethodArg=message;
+        this->writeMethodCallCount+=1;
+    }
+};
+
+class FakeCommunicator:public IMessageCommunicator
+{
+  public:
+    int  pushMessage(const string& message)
+    {
+       return 400;
+    }
+};
+
+class FakeSpeedSensor:public ISpeedSensor
+{
+   int getCurrentSpeed()
+   {
+       return 40;
+   }
+};
+
+TEST(VehicleSpeedMonitorTestSuite,AssertValidInstanceWithSpeedThreshold)
+{
+   int speedThreshold = 70;  // Sample threshold
+   SpeedMonitor monitor{speedThreshold};
+   EXPECT_EQ(monitor.getSpeedThreshold(), speedThreshold);
+}
+
+TEST(VehicleSpeedMonitorTestSuite, MonitorInteractWithLoggerWhenThresholdNotInRange)
+{
+    int speedThreshold = 150;  // Sample threshold
+    SpeedMonitor objUnderTest{speedThreshold};
+    MockLogger logger;//mockObject
+    FakeCommunicator communicator; //stub object
+    FakeSpeedSensor sensor; //stub object
+    objUnderTest.monitor(&communicator, &sensor, &logger);
+    EXPECT_GE(logger.writeMethodCallCount,1);
+    string  expectedMsgToBeRecvdByLoggerWrite="_speedThreshold value must be in the ramge {1-100} 150";
+    EXPECT_STREQ(writeMethodArg,expectedMsgToBeRecvdByLoggerWrite);
 }
